c29f33bf72f8a6325a960fd61fb0d3b085bfe2a6
Eduardo <edulink.1610@gmail.com>
2026-06-30 12:05:05 -0600

    Complete Sum All exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let result = [];
 
+    if(!Number.isInteger(first) || !Number.isInteger(second)) return 'ERROR';
+
+    if(first < 0 || second < 0) return 'ERROR';
+
+    if(second > first){
+        result.push(second);
+
+        while(second != first){
+            second -= 1;
+            result.push(second);
+        }
+    } else if(second < first){
+        result.push(first);
+
+        while(first != second){
+            first -= 1;
+            result.push(first);
+        }
+    } 
+
+    return result.reduce((sum, current) => sum + current, 0);
 };
 
 // Do not edit below this line
