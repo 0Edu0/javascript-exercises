b02e6d9d15a033e4c1c4134340beee126838d625
Eduardo <edulink.1610@gmail.com>
2026-07-08 17:41:59 -0600

    Complete shopping list exercise and create events examples

---
+++ external_exercises/dom/02-events/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+
+// const btn2 = document.querySelector("#btn2");
+// btn2.addEventListener("click", () => {
+// 	alert("Hello World!");
+// });
+
+// btn.addEventListener("click", function (e) {
+//   console.log(e);
+// });
+
+// btn.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
