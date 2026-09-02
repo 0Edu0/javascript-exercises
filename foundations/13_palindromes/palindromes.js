326d53f223450d3273b493a37ed84317cecac4ef
Eduardo <edulink.1610@gmail.com>
2026-09-01 22:04:54 -0600

    Complete exercise 13 - Palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const reverseStr = str.split("").reverse().join('').toLowerCase().replace(/[^a-z0-9]/g, "");
+    const strChecker = str => reverseStr === str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return strChecker(str);
+    // return str;
 };
 
 // Do not edit below this line
