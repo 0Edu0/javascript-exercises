8706b27dc26f0244268b243ac302a9a39c6c9b71
Eduardo <edulink.1610@gmail.com>
2026-09-01 21:31:05 -0600

    Complete exercise 12 - Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  const initialValue = 0;
+  return sumArray.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  const initialValue = 1;
+  return multiplyArray.reduce((accumulator, currentValue) => accumulator * currentValue, initialValue);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  const factorial = n => n <= 1 ? 1 : n * factorial(n - 1);
+  return factorial(n);
 };
 
 // Do not edit below this line
