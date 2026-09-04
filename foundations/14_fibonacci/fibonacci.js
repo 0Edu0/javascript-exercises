f34cc0b1a419feedf4893f031970d0229b66f720
Eduardo <edulink.1610@gmail.com>
2026-09-03 18:59:02 -0600

    Complete exercise 14 Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let first = 1;
+    let second = 1;
+    if(n == 0){
+        return 0;
+    } else if (n < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 1; i < n; i++){
+            let sum = first + second; 
+            second = first;
+            first = sum; 
+        };
+        return second;
+    };
 };
 
 // Do not edit below this line
